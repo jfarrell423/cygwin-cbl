85583988c28c10afd6b716a120442860906a405b
Jerry D Farrell <14806267+jfarrell423@users.noreply.github.com>
2023-11-17 08:40:15 -0500

    Adding some CYGWIN sample scripts.
    Adding some CYGWIN sample scripts.

---
+++ Sample-Shell-Script.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+echo -e "== SYSTEM INFO   =="
+echo -e "NAME:\t\t" `hostname`
+# echo -e "OS:\t"`hostnamectl | grep "Operating System" | cut -d ' ' -f5-`
+# Converted the line for use with CYGWIN
+echo -e "OS:\t\t" `uname`
+
+echo -e "== HARDWARE INFO =="
+echo -e "MEMORY:\t"`grep MemTotal /proc/meminfo`
+
+echo -e "== NETWORK INFO  =="
+#echo -e "System Main IP:\t\t"`hostname -I`
+echo -e "System Main IP:\n$(hostname -I | tr ' ' '\n')"
+
+
